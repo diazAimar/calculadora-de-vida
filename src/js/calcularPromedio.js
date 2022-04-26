42bf860899a1beddb1325d1454a7bf8ccc0f289e
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-04-26 19:48:02 -0300

    Aplicacion finalizada, se removio la funcionalidad de tema oscuro/claro por el agregado de imagenes de fondo. Cambio en estilos. Agregado de una alerta si algun input del formulario se encuentra incompleto.

--- src/js/calcularPromedio.js
+++ src/js/calcularPromedio.js
@@ -5,71 +5,63 @@ const form = document.getElementById('form');
 const divAlerta = document.getElementById('alerta');
 
 const chequearInputs = () => {
-	let proceda = false;
-	let clase = document.getElementById('clase').value;
-	let raza = document.getElementById('raza').value;
-	let nivel = Number(document.getElementById('nivel').value);
-	let vida = Number(document.getElementById('vida').value);
-	if (
-		clase !== 'Elegí una clase' &&
-		raza !== 'Elegí una raza' &&
-		nivel > 0 &&
-		vida > 0
-	) {
-		proceda = true;
-	}
-	return {proceda, clase, raza, nivel, vida};
+  let proceda = false;
+  let clase = document.getElementById('clase').value;
+  let raza = document.getElementById('raza').value;
+  let nivel = Number(document.getElementById('nivel').value);
+  let vida = Number(document.getElementById('vida').value);
+  if (clase !== 'Elegí una clase' && raza !== 'Elegí una raza' && nivel > 0 && vida > 0) {
+    proceda = true;
+  } else {
+    crearAlerta('Todos los campos deben estar completados.', 'warning');
+    setTimeout(() => {
+      let alertaActual = document.querySelector('.alerta-actual');
+      divAlerta.removeChild(alertaActual);
+    }, 3000);
+  }
+  return { proceda, clase, raza, nivel, vida };
 };
 
 const calcularVida = (clase, raza, nivel, vida) => {
-	// console.log('hi');
-	let vidaInicial = informacion[raza].vidaInicial;
-	let vidaPromedio =
-		(nivel - 1) * informacion[raza].promedio[clase] + vidaInicial;
+  // console.log('hi');
+  let vidaInicial = informacion[raza].vidaInicial;
+  let vidaPromedio = (nivel - 1) * informacion[raza].promedio[clase] + vidaInicial;
 
-	vida > vidaPromedio
-		? crearAlerta(
-				'Felicitaciones 🎉! Tu personaje esta +' + (vida - vidaPromedio) + '.',
-				'success'
-		  )
-		: vida == vidaPromedio
-		? crearAlerta('Tu vida esta en el promedio.', 'warning')
-		: crearAlerta(
-				'Tu vida está por debajo del promedio, ' + (vida - vidaPromedio) + '.',
-				'error'
-		  );
-	console.log(
-		`El promedio de vida de un ${clase} ${raza} nivel ${nivel} es de ${vidaPromedio}`
-	);
+  vida > vidaPromedio
+    ? crearAlerta('Felicitaciones 🎉! Tu personaje esta +' + (vida - vidaPromedio) + '.', 'success')
+    : vida == vidaPromedio
+    ? crearAlerta('Tu vida esta en el promedio.', 'warning')
+    : crearAlerta('Tu vida está por debajo del promedio, ' + (vida - vidaPromedio) + '.', 'error');
+  // console.log(`El promedio de vida de un ${clase} ${raza} nivel ${nivel} es de ${vidaPromedio}`);
 };
 
 const crearAlerta = (msg, tipo) => {
-	if (divAlerta.hasChildNodes()) {
-		let alertaActual = document.querySelector('.alerta-actual');
-		divAlerta.removeChild(alertaActual);
-	}
-	let alerta = document.createElement('div');
-	alerta.classList.add(
-		'alert',
-		`alert-${tipo}`,
-		'shadow-lg',
-		'alerta-actual',
-		'mt-5',
-		'text-center'
-	);
-	let texto = document.createTextNode(msg);
-	alerta.appendChild(texto);
-	divAlerta.appendChild(alerta);
+  if (divAlerta.hasChildNodes()) {
+    let alertaActual = document.querySelector('.alerta-actual');
+    divAlerta.removeChild(alertaActual);
+  }
+  let alerta = document.createElement('div');
+  alerta.classList.add(
+    'alert',
+    `alert-${tipo}`,
+    'shadow-lg',
+    'alerta-actual',
+    'mt-5',
+    'text-center'
+  );
+  let texto = document.createTextNode(msg);
+  alerta.appendChild(texto);
+  divAlerta.appendChild(alerta);
 };
 
 form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	/* Llamar a chequearInputs */
-	let {proceda, clase, raza, nivel, vida} = chequearInputs();
-	/* true ? calcularVida : alerta */
-	if (proceda) {
-		calcularVida(clase, raza, nivel, vida);
-	} else {
-		// console.log('bye');
-	}
+  e.preventDefault();
+  /* Llamar a chequearInputs */
+  let { proceda, clase, raza, nivel, vida } = chequearInputs();
+  /* true ? calcularVida : alerta */
+  if (proceda) {
+    calcularVida(clase, raza, nivel, vida);
+  } else {
+    // console.log('bye');
+  }
 });
--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -5,7 +5,7 @@ let changeThemeButton = document.getElementById('changeTheme');
 let html = document.documentElement;
 
 changeThemeButton.addEventListener('click', () => {
-	html.getAttribute('data-theme') == 'dark'
-		? html.setAttribute('data-theme', 'light')
-		: html.setAttribute('data-theme', 'dark');
+  html.getAttribute('data-theme') == 'dark'
+    ? html.setAttribute('data-theme', 'light')
+    : html.setAttribute('data-theme', 'dark');
 });
